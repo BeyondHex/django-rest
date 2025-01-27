8539cdbf313dc1275e9595f310bc30b5a6604020
Severin Gescher <Severin.Gescher@tsbw.cloud>
2025-01-27 14:19:09 +0100

    Fix Lint

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -52,7 +52,7 @@ class PublicUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short_error(self):
-        """Test an error is returned if the password is less than 5 characters."""
+        """Test an error is returned when less than 5 characters."""
         payload = {
             'email': 'test@example.com',
             'password': 'pw',
@@ -61,7 +61,8 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects.filter(email=payload['email']).exists()
+        user_exists = (get_user_model()
+                       .objects.filter(email=payload['email']).exists())
         self.assertFalse(user_exists)
 
     def test_create_token_for_user(self):
@@ -137,10 +138,7 @@ class PrivateUserApiTests(TestCase):
 
     def test_update_user_profile(self):
         """Test updating the user profile for authenticated user."""
-        payload = {
-            'name': 'Updated Name',
-            'password': 'newpassword123',
-        }
+        payload = {'name': 'Updated Name', 'password': 'newpassword123'}
 
         res = self.client.patch(ME_URL, payload)
 
